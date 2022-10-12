b2cda5b2b04e814878f489e4196cba8fa1710168
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-10-12 00:54:37 -0300

    Finalizado heading

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,42 +1,42 @@
 import type {ComponentMeta, ComponentStoryObj} from '@storybook/react';
 
-import {Text, type TextProps} from './Text';
+import {Heading, type HeadingProps} from './Heading';
 
 export default {
-  title: 'Components/Text',
-  component: Text,
+  title: 'Components/Heading',
+  component: Heading,
   args: {
     children: 'Lorem ipsum',
     size: 'md'
   },
   argTypes: {
     size: {
-      options: ['sm', 'md', 'lg'] as TextProps['size'][],
+      options: ['sm', 'md', 'lg'] as HeadingProps['size'][],
       control: {
         type: 'inline-radio'
       }
     }
   }
-} as ComponentMeta<typeof Text>
+} as ComponentMeta<typeof Heading>
 
-export const Default: ComponentStoryObj<typeof Text> = {}
+export const Default: ComponentStoryObj<typeof Heading> = {}
 
-export const Small: ComponentStoryObj<typeof Text> = {
+export const Small: ComponentStoryObj<typeof Heading> = {
   args: {
     size: 'sm'
   }
 };
 
-export const Large: ComponentStoryObj<typeof Text> = {
+export const Large: ComponentStoryObj<typeof Heading> = {
   args: {
     size: 'lg'
   }
 };
-export const CustomComponent: ComponentStoryObj<typeof Text> = {
+export const CustomComponent: ComponentStoryObj<typeof Heading> = {
   args: {
     asChild: true,
     children: (
-      <p>Lorem ipsum</p>
+      <h1>H1 heading</h1>
     )
   },
   argTypes: {
